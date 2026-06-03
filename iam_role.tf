4aa4c68102f8ee2dc6c67ca826b244f30186ecd3
AliceQuadrosWork <160643012+AliceQuadrosWork@users.noreply.github.com>
2026-06-03 10:40:28 +0200

    Create iam_role.tf

---
+++ iam_role.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_role" "dbt_execution_role" {
+  name = dbt_execution_role_tp
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+  })
+}
